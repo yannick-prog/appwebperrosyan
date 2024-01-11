a932539d56cd3ceaea50694e6fc2a2d24521e1b7
yannick-prog <151885268+yannick-prog@users.noreply.github.com>
2024-01-11 21:51:42 +0100

    antes de anadir vite

---
+++ resources/js/components/AñadirPerro/Añadir.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+    <h1>HOLA COMPONENTE PARA VER EL PRODUCTO</h1>
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/mas_perro.js
@@ -0,0 +1,5 @@
+import './bootstrap.js'
+import {createApp} from "vue";
+import MasPerro from "@/components/AñadirPerro/Añadir.vue";
+
+createApp(MasPerro).mount('#app');
